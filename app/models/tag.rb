4e7a402593216e92d42ca2cea8958affc3613950
Raghib Mahtab Quader <raghib.quader@gmail.com>
2018-04-25 18:56:42 -0400

    dependents destroyed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
-  has_many :taggings
-  has_many :articles, through: :taggings
+  has_many :taggings, dependent: :destroy
+  has_many :articles, through: :taggings, dependent: :destroy
 
   def to_s
     name
